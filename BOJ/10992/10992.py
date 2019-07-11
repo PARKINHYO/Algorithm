61dc944e2e73c34f534891d268e7253b2563ffd7
Your Name <you@example.com>
2019-07-11 12:36:31 +0900

    BOJ #10992: 별 찍기 - 17

---
+++ BOJ/10992/10992.py
@@ -0,0 +1,10 @@
+N = int(input())
+if N == 1:
+    print("*")
+else:
+    print(" "*(N-1) + "*")
+    for i in range(1, N):
+        if i == N-1:
+            print("*"*(2*N-1))
+        else:
+            print(" "*(N-i-1) + "*" + " "*(2*i-1) + "*")
