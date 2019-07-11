90aaa1e759f0989a5a6763a4233b0395ac2ee13b
Your Name <you@example.com>
2019-07-11 14:04:08 +0900

    BOJ #1546: 평균

---
+++ BOJ/1546/1546.py
@@ -0,0 +1,19 @@
+N = int(input())
+score = []
+score = input().split()
+score = [int (i) for i in score]
+score.sort()
+
+for i in range(0, N-1):
+    score[i] = float(score[i])/float(score[N-1])*100
+
+score[N-1] = 100
+sum = 0
+for i in range(0, N):
+    sum += float(score[i])
+
+average = float(sum)/N
+if len(str(average))>=10:
+    print("%0.6f" % average)
+else:
+    print("%0.2f" % average)
