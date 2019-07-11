a8522737d439e29d233daa13ab66c004f101bb38
Your Name <you@example.com>
2019-07-11 12:37:21 +0900

    BOJ #11651: 좌표 정렬하기 2

---
+++ BOJ/11651/11651.py
@@ -0,0 +1,9 @@
+N = int(input())
+YX = []
+for i in range(N):
+    x, y = map(int, input().split())
+    YX.append([y, x])
+YX.sort()
+for i in range(N):
+    print(YX[i][1], end=" ")
+    print(YX[i][0])
