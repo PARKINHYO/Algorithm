697971dc751f86ba620d28cbc00f4e562b50956d
Your Name <you@example.com>
2019-07-11 13:44:31 +0900

    BOJ #2908: 상수

---
+++ BOJ/2908/2908.py
@@ -0,0 +1,9 @@
+a, b = map(list, input().split())
+a.reverse()
+b.reverse()
+A = int(a[0])*100 + int(a[1])*10 +int(a[2])
+B = int(b[0])*100 + int(b[1])*10 +int(b[2])
+if A<B:
+    print(B)
+else:
+    print(A)
