e753aad156c298c58f1575831f4597ab69324d5b
Your Name <you@example.com>
2019-07-11 12:38:02 +0900

    BOJ #11399: ATM

---
+++ BOJ/11399/11399.py
@@ -0,0 +1,11 @@
+N = int(input())
+time = [int(x) for x in input().split()]
+time.sort()
+timesum = 0
+timesum2 = []
+timesum3 = 0
+for i in range(N):
+    timesum += time[i]
+    timesum2.append(timesum)
+    timesum3 += timesum2[i]
+print(timesum3)
