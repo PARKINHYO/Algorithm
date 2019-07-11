1a5219486062f548eeb0af49dad583b6c58b8bbb
Your Name <you@example.com>
2019-07-11 13:43:18 +0900

    BOJ #11022: A+B - 8

---
+++ BOJ/11022/11022.py
@@ -0,0 +1,4 @@
+T = int(input())
+AB = [[int(x) for x in input().split()] for y in range(T)]
+for i in range(T):
+    print("Case #%d: %d + %d = %d" % (i+1, AB[i][0], AB[i][1], AB[i][0]+AB[i][1]))
