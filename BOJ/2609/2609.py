e85ad9d7423da4709f46899dcdfb9d685f18c69c
Your Name <you@example.com>
2019-07-11 13:43:47 +0900

    BOJ #2609: 최대공약수와 최소공배수

---
+++ BOJ/2609/2609.py
@@ -0,0 +1,21 @@
+a, b = map(int, input().split())
+if a<b:
+    for i in range(a, 0, -1):
+        an = a%i
+        bn = b%i
+        am = a//i
+        bm = b//i
+        if an == bn == 0:
+            print(i)
+            print(i*am*bm)
+            break
+else:
+    for i in range(b, 0, -1):
+        an = a%i
+        bn = b%i
+        am = a//i
+        bm = b//i
+        if an == bn == 0:
+            print(i)
+            print(i*am*bm)
+            break
