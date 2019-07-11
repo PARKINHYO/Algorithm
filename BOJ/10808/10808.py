731bced47f7d1d4eed9cf4436d73eff40ea947d2
Your Name <you@example.com>
2019-07-11 12:36:24 +0900

    BOJ #10808: 알파벳 개수

---
+++ BOJ/10808/10808.py
@@ -0,0 +1,13 @@
+S = input()
+alphabet = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j', 11: 'k', 12: 'l', 13: 'm',
+            14: 'n', 15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 21: 'u', 22: 'v', 23: 'w', 24: 'x', 25: 'y',
+            26: 'z'}
+count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+for i in range(len(S)):
+    for j, k in alphabet.items():
+        if k == S[i]:
+            count[j-1] += 1
+        else:
+            continue
+for i in count:
+    print(i, end=" ")
