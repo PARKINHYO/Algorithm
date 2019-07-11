ae7d92c868d40a58ad5c92b3dbbc93b2998b112b
Your Name <you@example.com>
2019-07-11 14:04:15 +0900

    BOJ #4673: 셀프 넘버

---
+++ BOJ/4673/4673.py
@@ -0,0 +1,9 @@
+setSelf = set(i for i in range(1, 10001))
+setF = set()
+for i in range(1, 10001):
+    for j in str(i):
+        i += int(j)
+    setF.add(i)
+setSelf = sorted(setSelf - setF)
+for i in setSelf:
+    print(i)
