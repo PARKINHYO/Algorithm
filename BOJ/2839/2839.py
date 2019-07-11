81322aa94c2f82756605c2f99f998b477c38adb1
Your Name <you@example.com>
2019-07-11 14:04:46 +0900

    BOJ #2839: 설탕 배달

---
+++ BOJ/2839/2839.py
@@ -0,0 +1,12 @@
+n = int(input())
+F = n//5
+n%=5
+T = 0
+while F>=0:
+    if n%3 == 0:
+        T = n//3
+        n = n%3
+        break
+    F-=1
+    n+=5
+print((n==0) and (F+T) or -1)
