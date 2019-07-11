42e290ea8c16d0eee3ff01f33a8b758cce698c77
Your Name <you@example.com>
2019-07-11 13:45:06 +0900

    BOJ #2920: 음계

---
+++ BOJ/2920/2920.py
@@ -0,0 +1,9 @@
+list = [int(i) for i in input().split()]
+ascending = [1, 2, 3, 4, 5, 6, 7, 8]
+descending = [8, 7, 6, 5, 4, 3, 2, 1]
+if list == ascending:
+    print("ascending")
+elif list == descending:
+    print("descending")
+else:
+    print("mixed")
