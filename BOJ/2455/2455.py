cb8342124b67283858c1c1409c5dae120e61dfc4
Your Name <you@example.com>
2019-07-11 12:38:10 +0900

    BOJ #2455: 지능형 기차

---
+++ BOJ/2455/2455.py
@@ -0,0 +1,10 @@
+train = [[int(x) for x in input().split()] for y in range(4)]
+p1 = 0
+p2 = 0
+p3 = []
+for i in range(4):
+    p1 = -train[i][0] + train[i][1]
+    p2 += p1
+    p3.append(p2)
+p3.sort()
+print(p3[3])
