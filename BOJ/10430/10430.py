dab9624e4287467cac00748d620a3b423764d6c8
Your Name <you@example.com>
2019-07-11 14:06:24 +0900

    BOJ #10430: 나머지

---
+++ BOJ/10430/10430.py
@@ -0,0 +1,5 @@
+a, b, c = input().split(" ", 2)
+print((int(a)+int(b)) % int(c))
+print((int(a) % int(c) + int(b) % int(c)) % int(c))
+print(int(a)*int(b) % int(c))
+print((int(a) % int(c)*int(b)%int(c)) % int(c))
