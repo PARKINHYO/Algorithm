fde83f30cd212b299f2f42d135c037b7ac0cd18d
Your Name <you@example.com>
2019-07-11 12:36:17 +0900

    BOJ #1065: 한수

---
+++ BOJ/1065/1065.py
@@ -0,0 +1,20 @@
+def HanNumber(a):
+    if a>99:
+        b = a//100
+        c = a - b*100
+        d = c//10
+        e = c%10
+        if d == (b+e)/2:
+            return 1
+        else:
+            return 0
+
+    else:
+        return 1
+
+N = int(input())
+sum=0
+for i in range(1,N+1):
+    if HanNumber(i) == 1:
+        sum+=1
+print(sum)
