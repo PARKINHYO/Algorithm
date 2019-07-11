b7047d1e8225397492d2c5d9df20b561e0b53637
Your Name <you@example.com>
2019-07-11 12:37:49 +0900

    BOJ #2444: 별 찍기 - 7

---
+++ BOJ/2444/2444.py
@@ -0,0 +1,6 @@
+N = int(input())
+for i in range(1, N):
+    print(" "*(N-i) + "*"*(i-1) + "*" + "*"*(i-1))
+print("*"*(2*N-1))
+for i in range(N-1, 0, -1):
+    print(" " * (N - i) + "*" * (i - 1) + "*" + "*" * (i - 1))
