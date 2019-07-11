2b04de5f2e8030f78ed048acd44f1d5af3bfbabe
Your Name <you@example.com>
2019-07-11 13:44:38 +0900

    BOJ #2675: 문자열 반복

---
+++ BOJ/2675/2675.py
@@ -0,0 +1,12 @@
+S = int(input())
+R = [[x for x in input().split()] for y in range(S)]
+word=""
+word2=""
+word3=""
+for i in range(S):
+    word = R[i][1]
+    for j in range(len(word)):
+        word2 = word[j]*int(R[i][0])
+        word3+=word2
+    print(word3)
+    word3=""
