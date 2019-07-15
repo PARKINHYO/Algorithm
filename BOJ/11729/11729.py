abfeff788b307eb9bcee0542b23239e25e4c048b
Your Name <you@example.com>
2019-07-15 16:59:13 +0900

    BOJ #11729: 하노이 탑 이동 순서

---
+++ BOJ/11729/11729.py
@@ -0,0 +1,13 @@
+def hanoi(n, from_pos, to_pos, aux_pos):
+    if n == 1:
+        print(str(from_pos) + " " + str(to_pos))
+        return
+
+    hanoi(n - 1, from_pos, aux_pos, to_pos)
+    print(str(from_pos) + " " + str(to_pos))
+    hanoi(n - 1, aux_pos, to_pos, from_pos)
+
+
+N = int(input())
+print(pow(2, N) - 1)
+hanoi(N, 1, 3, 2)
