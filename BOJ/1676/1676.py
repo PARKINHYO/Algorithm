b3d16dfd2c1539f09ee314a806e3b64579fe5412
Your Name <you@example.com>
2019-07-15 16:58:59 +0900

    BOJ #1676: 팩토리얼 0의 개수

---
+++ BOJ/1676/1676.py
@@ -0,0 +1,20 @@
+def fact(n):
+    if n <= 1:
+        return 1
+    return n * fact(n - 1)
+
+
+def count_zero(a):
+    d = 10
+    count = 0
+    while (1):
+        if a % d == 0:
+            count += 1
+            d *= 10
+        else:
+            break
+    return count
+
+
+N = int(input())
+print(count_zero(fact(N)))
