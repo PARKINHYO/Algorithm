2873ab4fb495a012379e2d51bfcb35c1cd48473d
parkinhyo <inhyopark122@gmail.com>
2020-01-07 16:43:21 +0900

    boj

--- BOJ/11403/11403번(경로 찾기).py
+++ BOJ/11403/11403번(경로 찾기).py
@@ -7,46 +7,39 @@ class Graph:
     def __init__(self, vertices):
         self.graph = defaultdict(list)
         self.V = vertices
+        self.answer = []
+        self.before = 0
+
 
     def addEdge(self, u, v):
+
         self.graph[u].append(v)
 
+    def isEdge(self, u, v):
+        for i in self.graph[u]:
+            if i == v:
+                return True
+
+        return False
+
     def DFSUtil(self, v, visited):
+
         visited[v] = True
 
+        self.answer.append(v)
+
         for i in self.graph[v]:
-            if self.graph[i] == False:
-                self.DFSUtil(i, visited)
 
-    def DFS(self):
-        answer = [[0] * self.V] * self.V
-        visited = [False] * 105
-        count = 0
-
-        for i in range(self.V):
             if visited[i] == False:
-
                 self.DFSUtil(i, visited)
 
-                for k in range(len(visited)):
-                    if visited[k] == True:
-                        count += 1
-                if count == self.V:
-                    for j in range(len(answer)):
-                        answer[i][j] = 1
 
-                else:
-                    for j in range(len(answer)):
-                        if visited[j] == False:
-                            answer[i][j] = 0
-                        else:
-                            answer[i][j] = 1
+    def DFS(self, v):
 
-                visited = [False] * 105
-                count = 0
+        visited = [False] * 105
+        self.DFSUtil(v, visited)
 
-        return answer
-
+        return self.answer
 
 if __name__ == '__main__':
     V = int(stdin.readline())
@@ -55,6 +48,33 @@ if __name__ == '__main__':
         E = list(map(int, stdin.readline().split(" ")))
         for j in range(V):
             if E[j] == 1:
+
                 g.addEdge(i, j)
 
-    print(g.DFS())
+    answer = []
+
+    for i in range(0, V):
+
+        tmpAnswer = g.DFS(i)
+        tmpAnswer2 = [0]*V
+        for j in tmpAnswer:
+            tmpAnswer2[j] = 1
+            if i == j:
+                tmpAnswer2[j] = 0
+
+        for j in tmpAnswer:
+            tmp = g.isEdge(j, i)
+            if tmp == True:
+                tmpAnswer2[i] = 1
+
+        answer.append(tmpAnswer2)
+        while len(tmpAnswer) > 0 : tmpAnswer.pop()
+
+    for i in range(len(answer)):
+        for j in range(len(answer)):
+            print(answer[i][j], end="")
+            if j == len(answer)-1:
+                pass
+            else:
+                print(" ", end="")
+        print()
