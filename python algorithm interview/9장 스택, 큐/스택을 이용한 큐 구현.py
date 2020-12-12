613bb650e491e357b82ef27beede598e784fe66e
parkinhyo <inhyopark122@gmail.com>
2020-12-13 07:43:19 +0900

    Add 큐 구현, 9장 완료

---
+++ python algorithm interview/9장 스택, 큐/스택을 이용한 큐 구현.py
@@ -0,0 +1,23 @@
+import collections
+
+class MyQueue:
+    def __init__(self):
+        self.input = []
+        self.output = []
+
+    def push(self, x):
+        self.input.append(x)
+
+    def pop(self):
+        self.peek()
+        return self.output.pop()
+
+    def peek(self):
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+
+        return self.output[-1]
+
+    def empty(self):
+        return self.input == [] and self.output == []
