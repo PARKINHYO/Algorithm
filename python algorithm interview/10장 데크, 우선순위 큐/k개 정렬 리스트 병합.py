15a629879033306bc7685d3aaafa83d50d539b3e
parkinhyo <inhyopark122@gmail.com>
2020-12-13 07:44:16 +0900

    Add 원형 데크, k개 정렬 리스트 병합

---
+++ python algorithm interview/10장 데크, 우선순위 큐/k개 정렬 리스트 병합.py
@@ -0,0 +1,40 @@
+from typing import List
+import heapq
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        root = result = ListNode(None)
+        heap = []
+
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i, lists[i]))
+
+        while heap:
+            node = heapq.heappop(heap)
+            idx = node[1]
+            result.next =node[2]
+
+            result = result.next
+            if result.next:
+                heapq.heappush(heap, (result.next.val, idx, result.next))
+
+        return root.next
+
+l1 = ListNode(1)
+l1.next = ListNode(4)
+l1.next.next = ListNode(5)
+
+l2 = ListNode(1)
+l2.next = ListNode(3)
+l2.next.next = ListNode(4)
+
+l3 = ListNode(2)
+l3.next = ListNode(6)
+
+Solution().mergeKLists([l1, l2, l3])
