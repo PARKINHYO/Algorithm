b83fc005e4cc80d6dc2e804265a715696f5c542a
parkinhyo <inhyopark122@gmail.com>
2020-12-20 01:31:25 +0900

    Add 조합.py, Fix 순열.py

--- python algorithm interview/12장 그래프/순열.py
+++ python algorithm interview/12장 그래프/순열.py
@@ -1,28 +1,33 @@
 from typing import List
 import itertools
-# class Solution:
-#     def permute(self, nums: List[int]) -> List[List[int]]:
-#         results = []
-#         prev_elements = []
-#
-#         def dfs(elements):
-#             if len(elements) == 0:
-#                 results.append(prev_elements[:])
-#
-#             for e in elements:
-#                 next_elements = elements[:]
-#                 next_elements.remove(e)
-#
-#                 prev_elements.append(e)
-#                 dfs(next_elements)
-#                 prev_elements.pop()
-#
-#         dfs(nums)
-#         return results
 
 
 class Solution:
-    def permute(self, nums: List[int]):
-        return list(map(list,itertools.permutations(nums)))
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        prev_elements = []
 
-print(Solution().permute([1,2,3]))
+        def dfs(elements):
+            if len(elements) == 0:
+                results.append(prev_elements[:])
+                print(f'results : {results} dfs 종료')
+                print()
+
+            for e in elements:
+                next_elements = elements[:]
+                next_elements.remove(e)
+
+                prev_elements.append(e)
+                print(f'elements : {elements}, prev_elements : {prev_elements}, next_elements : {next_elements}, dfs 호출')
+                dfs(next_elements)
+                prev_elements.pop()
+
+        dfs(nums)
+        return results
+
+
+# class Solution:
+#     def permute(self, nums: List[int]):
+#         return list(map(list,itertools.permutations(nums)))
+# #
+# print(Solution().permute([1,2,3]))
---
+++ python algorithm interview/12장 그래프/조합.py
@@ -0,0 +1,25 @@
+from typing import List
+import itertools
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        results = []
+
+        def dfs(elements, start: int, k:int):
+            if k == 0:
+                results.append(elements[:])
+
+            for i in range(start, n+1):
+                elements.append(i)
+                print(f'range({start},{n+1}), elements: {elements}, dfs({i+1}, {k-1})')
+                dfs(elements, i+1, k-1)
+                elements.pop()
+
+        dfs([], 1, k)
+        return results
+# class Solution:
+#     def combine(self, n: int, k: int):
+#         return list(itertools.combinations(range(1, n+1), k))
+
+Solution().combine(4,2)
