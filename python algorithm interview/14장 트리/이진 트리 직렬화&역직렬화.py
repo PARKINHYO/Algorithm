59637710aa40a05c6738a6c0d6ce6f421180f583
parkinhyo <inhyopark122@gmail.com>
2021-01-01 18:00:21 +0900

    균형 이진 트리, 이진 트리 직렬화&역직렬화, 최소 높이 트리

---
+++ python algorithm interview/14장 트리/이진 트리 직렬화&역직렬화.py
@@ -0,0 +1,82 @@
+import collections
+
+
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Codec:
+    # 직렬화
+    def serialize(self, root: TreeNode) -> str:
+        queue = collections.deque([root])
+        result = ['#']
+        # 트리 BFS 직렬화
+        while queue:
+            node = queue.popleft()
+            if node:
+                queue.append(node.left)
+                queue.append(node.right)
+
+                result.append(str(node.val))
+            else:
+                result.append('#')
+        return  ' '.join(result)
+
+    def deserialize(self, data: str) -> TreeNode:
+        # 예외 처리
+        if data == '# #':
+            return None
+
+        nodes = data.split()
+
+        root = TreeNode(int(nodes[1]))
+        queue = collections.deque([root])
+        index = 2
+
+        while queue:
+            node = queue.popleft()
+            if nodes[index] is not '#':
+                node.left = TreeNode(int(nodes[index]))
+                queue.append(node.left)
+            index += 1
+
+
+            if nodes[index] is not '#':
+                node.right = TreeNode(int(nodes[index]))
+                queue.append(node.right)
+            index += 1
+
+        return root
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
