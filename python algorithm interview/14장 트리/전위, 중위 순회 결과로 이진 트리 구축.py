00abc998cf4d35e7c40bbea8dffd9f187883686b
parkinhyo <inhyopark122@gmail.com>
2021-01-04 02:03:44 +0900

    14장 트리 완료.

---
+++ python algorithm interview/14장 트리/전위, 중위 순회 결과로 이진 트리 구축.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if inorder:
+            # 전위 순회 결과는 중위 순회 분할 인덱스
+            index = inorder.index(preorder.pop(0))
+
+            # 중위 순회 결과 분할 정복
+            node = TreeNode(inorder[index])
+            node.left = self.buildTree(preorder, inorder[0:index])
+            node.right = self.buildTree(preorder, inorder[index + 1:])
+
+
+            return node
