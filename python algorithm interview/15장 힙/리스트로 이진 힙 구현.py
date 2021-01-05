b4cc7c518aed7f160a3851168070f5c7fd427b3e
parkinhyo <inhyopark122@gmail.com>
2021-01-05 13:47:44 +0900

    Add 이진 힙 구현

---
+++ python algorithm interview/15장 힙/리스트로 이진 힙 구현.py
@@ -0,0 +1,52 @@
+class BinaryHeap(object):
+    def __init__(self):
+        self.items = [None]
+
+
+    def __len__(self):
+        return len(self.items)
+
+
+    # 삽입 시 실행, 반복 구조 구현
+    def _percolate_up(self):
+        i = len(self)
+        parent = i // 2
+        while parent > 0:
+            if self.items[i] < self.items[parent]:
+                self.items[parent], self.items[i] = self.items[i], self.items[parent]
+
+            i = parent
+            parent = i // 2
+
+    def insert(self, k):
+        self.items.append(k)
+        self._percolate_up()
+
+
+    # 추출시 실행, 재귀 구조 구현
+    def _percolate_down(self, idx):
+        left = idx * 2
+        right = idx * 2 + 1
+        smallest = idx
+
+
+        if left <= len(self) and self.items[left] < self.items[smallest]:
+            smallest = left
+
+
+        if right <= len(self) and self.items[right] < self.items[smallest]:
+            smallest = right
+
+
+        if smallest != idx:
+            self.items[idx], self.items[smallest] = self.items[smallest], self.items[idx]
+            self._percolate_down(smallest)
+
+
+    def extract(self):
+        extracted = self.items[1]
+        self.items[1] = self.items[len(self)]
+        self.items.pop()
+        self._percolate_down(1)
+        return extracted
+
