4f1296c2358e757a27b0fca7c03f755d1f809094
parkinhyo <inhyopark122@gmail.com>
2021-01-22 01:18:09 +0900

    Add Q58.

---
+++ python algorithm interview/17장 정렬/Q58. 리스트 정렬 - 병합.py
@@ -0,0 +1,30 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+
+
+            l1.next = self.mergeTwoLists(l1.next, l2)
+
+        return l1 or l2
+
+    def sortList(self, head: ListNode) -> ListNode:
+        if not (head and head.next):
+            return head
+
+        half, slow, fast = None, head, head
+        while fast and fast.next:
+            half, slow, fast = slow, slow.next, fast.next.next
+        half.next = None
+
+        l1 = self.sortList(head)
+        l2 = self.sortList(slow)
+
+        return self.mergeTwoLists(l1, l2)
---
+++ python algorithm interview/17장 정렬/bubblesort.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+def bubble_sort(l: List):
+    for i in range(len(l)):
+        for j in range(len(l) - 1):
+            if l[j] > l[j + 1]:
+                l[j], l[j + 1] = l[j + 1], l[j]
+
+
+def quicksort(A, lo, hi):
+    def partition(lo, hi):
+        pivot = A[hi]
+        left = lo
+        for right in range(lo, hi):
+            if A[right] < pivot:
+                A[left], A[right] = A[right], A[left]
+                left += 1
+
+        A[left], A[hi] = A[hi], A[left]
+        return left
+
+    if lo < hi:
+        pivot = partition(lo, hi)
+        quicksort(A, lo, pivot - 1)
+        quicksort(A, pivot + 1, hi)
