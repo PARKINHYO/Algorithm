27838a6ea7479e82523f617ea41bd3b1c01b4649
parkinhyo <inhyopark122@gmail.com>
2021-04-23 17:47:36 +0900

    boj 2696.

--- BOJ/2696/2696.py
+++ BOJ/2696/2696.py
@@ -5,28 +5,45 @@ from typing import List
 class Solution:
     def centerValue(self, M: int, data: List[int]):
 
-        left = 0
         answers = []
         for right in range(1, M + 1, 2):
-            suffix = data[left:right]
+            suffix = data[:right]
             suffix.sort()
-            answers.append(suffix[left + (right - left) // 2])
+            answers.append(suffix[right // 2])
 
         if len(answers) <= 10:
-            print(len(answers))
-            print(answers)
+            print(len(answers)) 
+            print(*answers)
         else:
-            count = 0
+            print(len(answers))
+            linedAnswers = []
+            tmp = []
             for answer in answers:
-                if count == 10:
-                    print()
-                    count = 0
-                print(answer, end=" ")
-                count += 1
+                if len(tmp) == 10:
+                    linedAnswers.append(tmp)
+                    tmp = []
+                tmp.append(answer)
+            linedAnswers.append(tmp)
+            for linedAnswer in linedAnswers:
+                print(*linedAnswer)
 
         return
 
 
 for _ in range(int(stdin.readline())):
     M = int(stdin.readline())
-    Solution().centerValue(M, list(map(int, stdin.readline().split())))
+
+    if M <= 10:
+        Solution().centerValue(M, list(map(int, stdin.readline().split())))
+    else:
+        data = []
+        a = M // 10
+        b = M % 10
+        if b == 0:
+            for i in range(a):
+                data.extend(list(map(int, stdin.readline().split())))
+            Solution().centerValue(M, data)
+        else:
+            for i in range(a+1):
+                data.extend(map(int, stdin.readline().split()))
+            Solution().centerValue(M, data) 
