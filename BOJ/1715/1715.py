74c609beabc8ad80e393a266b9bf10e6b9419f87
parkinhyo <inhyopark122@gmail.com>
2021-06-01 18:52:34 +0900

    Update BOJ #1715 카드 정렬하기

--- BOJ/1715/1715.py
+++ BOJ/1715/1715.py
@@ -9,14 +9,13 @@ for _ in range(N):
     heapq.heappush(Q, int(input()))
 
 if N == 1:
-    print(heapq.heappop(Q))
+    print(0)
     exit(0)
     print(heapq.heappop(Q) + heapq.heappop(Q))
     exit(0)
 else:
     answers = []
     while Q and len(Q) != 1:
-        print(answers)
         answers.append(heapq.heappop(Q) + heapq.heappop(Q))
         heapq.heappush(Q, answers[-1])
 print(sum(answers))
