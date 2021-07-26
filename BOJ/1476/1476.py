6e349149a04802a566a5a005615d7fcf44041eb2
parkinhyo <inhyopark122@gmail.com>
2021-07-26 23:53:36 +0900

    BOJ #1476: 날짜 계산

---
+++ BOJ/1476/1476.py
@@ -0,0 +1,8 @@
+E, S, M = map(int, input().split())
+year = 1
+ 
+while True:
+    if (year - E) % 15 == 0 and (year - S) % 28 == 0 and (year - M) % 19 == 0:
+        print(year)
+        break
+    year += 1
